0e1f865ad2d59d029e7fee8d4b52dab0783654be
ervinkacar1991 <kacarervin@gmail.com>
2022-07-25 12:23:53 +0200

    redux learning

---
+++ src/components/Login.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { login, logout } from "../features/user";
+
+function Login() {
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          dispatch(login({ name: "Ervin", age: 30, email: "kacar@gmail.com" }));
+        }}
+      >
+        Login
+      </button>
+      <button
+        onClick={() => {
+          dispatch(logout());
+        }}
+      >
+        Logout
+      </button>
+    </div>
+  );
+}
+
+export default Login;
